baacda88a477bbdb69e81ee57595881bcf11a6cd
hardhikgupta <hardhikgupta@hotmail.com>
2021-11-21 12:55:46 +0530

    codr

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//var cid = "UCgUmIK6sSgyuKp8fHrLFGMQ";
+//gvar cid = "UCgUmIK6sSgyuKp8fHrLFGMQ";
 
 var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=");
 let iframes = document.getElementById('latestVideoEmbed')
